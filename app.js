bf8b1ecc62706f642fd88f4f67589158f4db1d40
aldigelos <aldi.gelos@gmail.com>
2026-09-21 20:54:09 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+let cart = [];
+
+const buttons = document.querySelectorAll(".product button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+
+        const product = button.parentElement;
+
+        const name = product.querySelector("h3").textContent;
+        const priceText = product.querySelector("span").textContent;
+
+        const price = Number(
+            priceText.replace("$", "").replace(".", "")
+        );
+
+        cart.push({
+            name: name,
+            price: price
+        });
+
+        alert(`${name} fue agregado al carrito ☕`);
+
+        console.log("Carrito:", cart);
+    });
+});
