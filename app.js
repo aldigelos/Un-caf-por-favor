c3729da351254c6e1e2fc1c04ae7f5c74f1b347c
aldigelos <aldi.gelos@gmail.com>
2026-09-21 20:59:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ buttons.forEach((button) => {
             name: name,
             price: price
         });
-
+        document.getElementById("cart-count").textContent = cart.length;
         alert(`${name} fue agregado al carrito ☕`);
 
         console.log("Carrito:", cart);
